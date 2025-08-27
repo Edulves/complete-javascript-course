27ec7314fc5a4ff285e3785bdf411028e4c13d3b
EduardoAMVOX <118478397+EduardoAMVOX@users.noreply.github.com>
2025-08-27 06:32:21 -0300

    Base dos projetos e aulas

---
+++ 10-Functions/starter/script.js
@@ -0,0 +1,61 @@
+'use strict';
+/*
+const bookings = [];
+
+const createBooking = function (
+  flightNum,
+  numPassengers = 1,
+  price = 199 * numPassengers
+) {
+  // ES
+  //   numPassengers = numPassengers || 1;
+  //   price = price || 199;
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('LH123', 2);
+createBooking('LH124', undefined, 1000);
+createBooking('LH124', undefined, 1000);
+createBooking('LH124', undefined, 1000);
+
+createBooking('LH124', undefined, 1000);
+
+*/
+
+const flight = 'LH234';
+const jonas = {
+  name: 'Jonas Schmedtmann',
+  passport: 24739479284,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr. ' + passenger.name;
+
+  if (passenger.passport === 24739479284) {
+    alert('Check in');
+  } else {
+    alert('Wrong passport!');
+  }
+};
+
+// checkIn(flight, jonas);
+// console.log(flight);
+// console.log(jonas);
+
+// Is the same as doing...
+// const flightNum = flight;
+// const passenger = jonas;
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 100000000000000);
+};
+
+newPassport(jonas);
+checkIn(flight, jonas);
