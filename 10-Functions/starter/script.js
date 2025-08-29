842007d31477ddc913fee3c38cae2c0cdbd40283
EduardoAMVOX <118478397+EduardoAMVOX@users.noreply.github.com>
2025-08-28 22:13:09 -0300

    142. CHALLENGE #1 Not completed

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,5 +1,8 @@
 'use strict';
+
 /*
+///////////////////////////////////////
+// Default Parameters
 const bookings = [];
 
 const createBooking = function (
@@ -7,9 +10,10 @@ const createBooking = function (
   numPassengers = 1,
   price = 199 * numPassengers
 ) {
-  // ES
-  //   numPassengers = numPassengers || 1;
-  //   price = price || 199;
+  // ES5
+  // numPassengers = numPassengers || 1;
+  // price = price || 199;
+
   const booking = {
     flightNum,
     numPassengers,
@@ -19,15 +23,16 @@ const createBooking = function (
   bookings.push(booking);
 };
 
+createBooking('LH123');
+createBooking('LH123', 2, 800);
 createBooking('LH123', 2);
-createBooking('LH124', undefined, 1000);
-createBooking('LH124', undefined, 1000);
-createBooking('LH124', undefined, 1000);
+createBooking('LH123', 5);
 
-createBooking('LH124', undefined, 1000);
+createBooking('LH123', undefined, 1000);
 
-*/
-/*
+
+///////////////////////////////////////
+// How Passing Arguments Works: Values vs. Reference
 const flight = 'LH234';
 const jonas = {
   name: 'Jonas Schmedtmann',
@@ -39,7 +44,7 @@ const checkIn = function (flightNum, passenger) {
   passenger.name = 'Mr. ' + passenger.name;
 
   if (passenger.passport === 24739479284) {
-    alert('Check in');
+    alert('Checked in');
   } else {
     alert('Wrong passport!');
   }
@@ -54,59 +59,65 @@ const checkIn = function (flightNum, passenger) {
 // const passenger = jonas;
 
 const newPassport = function (person) {
-  person.passport = Math.trunc(Math.random() * 100000000000000);
+  person.passport = Math.trunc(Math.random() * 100000000000);
 };
 
 newPassport(jonas);
 checkIn(flight, jonas);
 
 
-const oneWord = function(str) {
+///////////////////////////////////////
+// Functions Accepting Callback Functions
+const oneWord = function (str) {
   return str.replace(/ /g, '').toLowerCase();
-}
+};
 
-const upperFirstWord = function(str) {
+const upperFirstWord = function (str) {
   const [first, ...others] = str.split(' ');
   return [first.toUpperCase(), ...others].join(' ');
-}
+};
 
 // Higher-order function
-const transformer = function(str, fn) {
+const transformer = function (str, fn) {
   console.log(`Original string: ${str}`);
   console.log(`Transformed string: ${fn(str)}`);
 
   console.log(`Transformed by: ${fn.name}`);
-}
+};
 
 transformer('JavaScript is the best!', upperFirstWord);
 transformer('JavaScript is the best!', oneWord);
 
 // JS uses callbacks all the time
-const high5 = function() {
+const high5 = function () {
   console.log('👋');
-}
+};
 document.body.addEventListener('click', high5);
-
 ['Jonas', 'Martha', 'Adam'].forEach(high5);
 
+
+///////////////////////////////////////
+// Functions Returning Functions
 const greet = function (greeting) {
   return function (name) {
     console.log(`${greeting} ${name}`);
   };
 };
 
-const greeterhey = greet('Hey');
-greeterhey('Jonas');
-greeterhey('Steven');
+const greeterHey = greet('Hey');
+greeterHey('Jonas');
+greeterHey('Steven');
 
 greet('Hello')('Jonas');
 
 // Challenge
 const greetArr = greeting => name => console.log(`${greeting} ${name}`);
 
-greetArr('Yoh')('Eduardo');
-*/
+greetArr('Hi')('Jonas');
 
+
+///////////////////////////////////////
+// The call and apply Methods
 const lufthansa = {
   airline: 'Lufthansa',
   iataCode: 'LH',
@@ -155,9 +166,9 @@ book.apply(swiss, flightData);
 console.log(swiss);
 
 book.call(swiss, ...flightData);
-console.log(swiss);
 
-// bind method
+///////////////////////////////////////
+// The bind Method
 // book.call(eurowings, 23, 'Sarah Williams');
 
 const bookEW = book.bind(eurowings);
@@ -166,13 +177,13 @@ const bookLX = book.bind(swiss);
 
 bookEW(23, 'Steven Williams');
 
-const bookEW23 = book.bind(eurowings, 12345);
-bookEW23('Eduardo Alves');
+const bookEW23 = book.bind(eurowings, 23);
 bookEW23('Jonas Schmedtmann');
+bookEW23('Martha Cooper');
 
 // With Event Listeners
 lufthansa.planes = 300;
-lufthansa.buyPlane = function(){
+lufthansa.buyPlane = function () {
   console.log(this);
 
   this.planes++;
@@ -180,25 +191,87 @@ lufthansa.buyPlane = function(){
 };
 // lufthansa.buyPlane();
 
-document.querySelector('.buy').addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+document
+  .querySelector('.buy')
+  .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
 
 // Partial application
-
 const addTax = (rate, value) => value + value * rate;
-console.log(addTax(0.10, 200));
+console.log(addTax(0.1, 200));
 
 const addVAT = addTax.bind(null, 0.23);
-// addVat = value => value + value * 0.23;
+// addVAT = value => value + value * 0.23;
 
 console.log(addVAT(100));
 console.log(addVAT(23));
 
-
-const addTaxRate = function(rate) {
-  return function(value) {
+const addTaxRate = function (rate) {
+  return function (value) {
     return value + value * rate;
-  }
-}
+  };
+};
 const addVAT2 = addTaxRate(0.23);
 console.log(addVAT2(100));
 console.log(addVAT2(23));
+*/
+
+///////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+Let's build a simple poll app!
+
+A poll has a question, an array of options from which people can choose, and an array with the number of replies for each option. This data is stored in the starter object below.
+
+Here are your tasks:
+
+1. Create a method called 'registerNewAnswer' on the 'poll' object. The method does 2 things:
+  1.1. Display a prompt window for the user to input the number of the selected option. The prompt should look like this:
+        What is your favourite programming language?
+        0: JavaScript
+        1: Python
+        2: Rust
+        3: C++
+        (Write option number)
+  
+  1.2. Based on the input number, update the answers array. For example, if the option is 3, increase the value AT POSITION 3 of the array by 1. Make sure to check if the input is a number and if the number makes sense (e.g answer 52 wouldn't make sense, right?)
+2. Call this method whenever the user clicks the "Answer poll" button.
+3. Create a method 'displayResults' which displays the poll results. The method takes a string as an input (called 'type'), which can be either 'string' or 'array'. If type is 'array', simply display the results array as it is, using console.log(). This should be the default option. If type is 'string', display a string like "Poll results are 13, 2, 4, 1". 
+4. Run the 'displayResults' method at the end of each 'registerNewAnswer' method call.
+
+HINT: Use many of the tools you learned about in this and the last section 😉
+
+BONUS: Use the 'displayResults' method to display the 2 arrays in the test data. Use both the 'array' and the 'string' option. Do NOT put the arrays in the poll object! So what shoud the this keyword look like in this situation?
+
+BONUS TEST DATA 1: [5, 2, 3]
+BONUS TEST DATA 2: [1, 5, 3, 9, 6, 1]
+
+GOOD LUCK 😀
+*/
+
+const poll = {
+  question: 'What is your favourite programming language?',
+  options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+  // This generates [0, 0, 0, 0]. More is the next section 😀
+  answers: new Array(4).fill(0),
+  registerNewAnswer: function () {
+    const choice = prompt(
+      'What is your favourite programming language?\n0: JavaScript\n1: Python\n2: Rust\n3: C++\n (Write option number)'
+    );
+    if (
+      isNaN(Number(choice)) ||
+      choice.trim() == '' ||
+      choice < 0 ||
+      choice > 3
+    ) {
+      console.log('Invalid input');
+      return;
+    }
+    this.answers[choice]++;
+    console.log(this.answers);
+  },
+};
+
+document
+  .querySelector('.poll')
+  .addEventListener('click', poll.registerNewAnswer.bind(poll));
