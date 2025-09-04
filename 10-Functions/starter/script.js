f2377f3713f26299d8987079275c7ecb216a5ddc
EduardoAMVOX <118478397+EduardoAMVOX@users.noreply.github.com>
2025-09-03 21:27:24 -0300

    145. More Closure Examples

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -312,6 +312,7 @@ runOnce();
 (() => console.log('This will never ALSO run again'))();
 */
 
+/*
 const secureBooking = function () {
   let passengerCount = 0;
 
@@ -328,3 +329,43 @@ booker();
 booker();
 
 console.dir(booker);
+*/
+
+// Example 1
+let f;
+
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
+g();
+f();
+
+// Re-assigning f function
+h();
+f();
+console.dir(f);
+
+// Example 2
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers`);
+  }, wait * 1000);
+
+  console.log(`Will start boarding in ${wait} seconds`);
+};
+
+boardPassengers(180, 3);
