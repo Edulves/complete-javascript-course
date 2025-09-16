61bd2d7dd1ac05f1527f1a3617cc556901d2ee0a
EduardoAMVOX <118478397+EduardoAMVOX@users.noreply.github.com>
2025-09-16 20:40:58 -0300

    174. More Ways of Creating and Filling Arrays

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -727,7 +727,7 @@ console.log(movements);
 
 /////////////////////////////////////
 // Array Grouping
-
+/*
 console.log(movements);
 
 const groupedMovements = Object.groupBy(movements, movement =>
@@ -747,3 +747,45 @@ console.log(groupedByActivity);
 
 const groupedAccounts = Object.groupBy(accounts, ({ type }) => type);
 console.log(groupedAccounts);
+*/
+
+const arr = [1, 2, 3, 4, 5, 6, 7];
+console.log(new Array(1, 2, 3, 4, 5, 6, 7));
+
+// Empty arrays + fill method
+const x = new Array(7);
+console.log(x);
+// console.log(x.map(() => 5));
+x.fill(1, 3, 5);
+x.fill(1);
+console.log(x);
+
+arr.fill(23, 4, 6);
+console.log(arr);
+
+// Array.from
+const y = Array.from({ length: 7 }, () => 1);
+console.log(y);
+
+const z = Array.from({ length: 7 }, (_, index) => index + 1);
+console.log(z);
+
+const diceRolls = Array.from({ length: 100 }, () =>
+  Math.round(Math.random() * 100)
+);
+console.log(diceRolls);
+
+console.log('**');
+
+labelBalance.addEventListener('click', function () {
+  const movementsUI = Array.from(
+    document.querySelectorAll('.movements__value'),
+    el => Number(el.textContent.replace('€'))
+  );
+  console.log(movementsUI);
+
+  const movementsUI2 = [...document.querySelectorAll('.movements__value')]/*.map(
+    el => Number(el.textContent.replace('€'))*/
+  );
+  console.log(movementsUI2);
+});
