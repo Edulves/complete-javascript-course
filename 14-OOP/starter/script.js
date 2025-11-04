fb95a39b9265ddfa19c27d79eb332388232790df
EduardoAMVOX <118478397+EduardoAMVOX@users.noreply.github.com>
2025-11-04 19:07:17 -0300

    Constructor Functions and the new Operator

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const Person = function (firstName, birthYear) {
+  // Instance properties
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  // Never do this
+  //   this.calcAge = function () {
+  //     console.log(2037 - this.birthYear);
+  //   };
+};
+
+const eduardo = new Person('Eduardo', 1998);
+console.log(eduardo);
+
+// 1. New {} is created
+// 2. function is called, this = {}
+// 3. {} linked to prototype
+// 4. function automatically return {}
+
+const matilda = new Person('Matilda', 2017);
+const jack = new Person('Jack', 1975);
+console.log(matilda, jack);
+
+console.log(jonas instanceof Person);
