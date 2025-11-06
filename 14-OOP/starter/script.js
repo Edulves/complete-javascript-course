7d0cbc074ed173a144e6984e3bcb8a0fcef45aad
EduardoAMVOX <118478397+EduardoAMVOX@users.noreply.github.com>
2025-11-05 21:53:35 -0300

    222. Prototypes

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,5 +1,7 @@
 'use strict';
 
+//////////////////////////////////
+// Constructor Functions and the new Operator
 const Person = function (firstName, birthYear) {
   // Instance properties
   this.firstName = firstName;
@@ -23,4 +25,29 @@ const matilda = new Person('Matilda', 2017);
 const jack = new Person('Jack', 1975);
 console.log(matilda, jack);
 
-console.log(jonas instanceof Person);
+console.log(eduardo instanceof Person);
+
+// Prototypes
+console.log(Person.prototype);
+
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+
+eduardo.calcAge();
+matilda.calcAge();
+
+console.log(eduardo.__proto__);
+console.log(eduardo.__proto__ === Person.prototype);
+
+console.log(Person.prototype.isPrototypeOf(eduardo));
+console.log(Person.prototype.isPrototypeOf(matilda));
+console.log(Person.prototype.isPrototypeOf(Person));
+
+// .prototypeOfLinkedObjects
+
+Person.prototype.species = 'Homo Sapiens';
+console.log(eduardo.species, matilda.species);
+
+console.log(eduardo.hasOwnProperty('firstName'));
+console.log(eduardo.hasOwnProperty('species'));
