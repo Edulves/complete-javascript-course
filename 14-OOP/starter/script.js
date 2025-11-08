7ceae4f95750889e817cfcb3d8c85c84818291fc
EduardoAMVOX <118478397+EduardoAMVOX@users.noreply.github.com>
2025-11-08 18:50:33 -0300

    225. CHALLENGE #1

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -14,7 +14,7 @@ const Person = function (firstName, birthYear) {
 };
 
 const eduardo = new Person('Eduardo', 1998);
-console.log(eduardo);
+// console.log(eduardo);
 
 // 1. New {} is created
 // 2. function is called, this = {}
@@ -23,31 +23,99 @@ console.log(eduardo);
 
 const matilda = new Person('Matilda', 2017);
 const jack = new Person('Jack', 1975);
-console.log(matilda, jack);
+// console.log(matilda, jack);
 
-console.log(eduardo instanceof Person);
+// console.log(eduardo instanceof Person);
 
 // Prototypes
-console.log(Person.prototype);
+// console.log(Person.prototype);
 
 Person.prototype.calcAge = function () {
   console.log(2037 - this.birthYear);
 };
 
-eduardo.calcAge();
-matilda.calcAge();
+// eduardo.calcAge();
+// matilda.calcAge();
 
-console.log(eduardo.__proto__);
-console.log(eduardo.__proto__ === Person.prototype);
+// console.log(eduardo.__proto__);
+// console.log(eduardo.__proto__ === Person.prototype);
 
-console.log(Person.prototype.isPrototypeOf(eduardo));
-console.log(Person.prototype.isPrototypeOf(matilda));
-console.log(Person.prototype.isPrototypeOf(Person));
+// console.log(Person.prototype.isPrototypeOf(eduardo));
+// console.log(Person.prototype.isPrototypeOf(matilda));
+// console.log(Person.prototype.isPrototypeOf(Person));
 
 // .prototypeOfLinkedObjects
 
 Person.prototype.species = 'Homo Sapiens';
-console.log(eduardo.species, matilda.species);
+// console.log(eduardo.species, matilda.species);
 
-console.log(eduardo.hasOwnProperty('firstName'));
-console.log(eduardo.hasOwnProperty('species'));
+// console.log(eduardo.hasOwnProperty('firstName'));
+// console.log(eduardo.hasOwnProperty('species'));
+
+// console.log(eduardo.__proto__);
+// Object.prototype (top of prototype chain)
+// console.log(eduardo.__proto__.__proto__);
+// console.log(eduardo.__proto__.__proto__.__proto__);
+
+// console.dir(Person.prototype.constructor);
+
+const arr = [3, 6, 4, 5, 6, 9, 9]; // === new Array === []
+// console.log(arr.__proto__);
+// console.log(arr.__proto__ === Array.prototype);
+
+// console.log(arr.__proto__.__proto__);
+
+Array.prototype.unique = function () {
+  return [...new Set(this)];
+};
+
+// console.log(arr.unique());
+
+const h1 = document.querySelector('h1');
+// console.dir(x => x + 1);
+
+///////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+1. Use a constructor function to implement a Car. A car has a make and a speed property. The speed property is the current speed of the car in km/h;
+2. Implement an 'accelerate' method that will increase the car's speed by 10, and log the new speed to the console;
+3. Implement a 'brake' method that will decrease the car's speed by 5, and log the new speed to the console;
+4. Create 2 car objects and experiment with calling 'accelerate' and 'brake' multiple times on each of them.
+
+DATA CAR 1: 'BMW' going at 120 km/h
+DATA CAR 2: 'Mercedes' going at 95 km/h
+
+GOOD LUCK 😀
+*/
+
+const Car = function (make, speed) {
+  this.name = make;
+  this.speed = speed;
+};
+
+Car.prototype.Accelerate = function () {
+  this.speed += 10;
+  console.log(`${this.name} Accelerated: ${this.speed}`);
+};
+
+Car.prototype.Brake = function () {
+  this.speed -= 5;
+  console.log(`${this.name} braked: ${this.speed}`);
+};
+
+const BMW = new Car('BMW', 120);
+console.log(BMW);
+
+BMW.Accelerate();
+BMW.Accelerate();
+BMW.Brake();
+BMW.Brake();
+
+const Mercedes = new Car('Mercedes', 95);
+console.log(Mercedes);
+
+Mercedes.Accelerate();
+Mercedes.Accelerate();
+Mercedes.Brake();
+Mercedes.Brake();
