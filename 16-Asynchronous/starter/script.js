0099e6383150356e98e3a8d1d5e460fc437fd248
Eduardo Alves <Eduardo.lves@outlook.com>
2025-12-06 18:18:42 -0300

    267. Throwing Errors Manually

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -113,23 +113,34 @@ const renderError = function (msg) {
 //     });
 // };
 
+const getJSON = function (url, errorMsg = 'Something went wrong') {
+  return fetch(url).then(response => {
+    if (!response.ok) throw new Error(`${errorMsg} (${response.status})`);
+
+    return response.json();
+  });
+};
+
 const getCountryData = function (country) {
-  fetch(`https://restcountries.com/v3.1/name/${country}`)
-    .then(response => response.json())
+  getJSON(`https://restcountries.com/v3.1/name/${country}`, 'Country not found')
     .then(data => {
       renderCountry(data[0]);
-      const neighbour = data[0].borders[0];
+      const neighbour = data[0]?.borders?.[0] ?? null;
 
-      if (!neighbour) return;
+      if (!neighbour) throw new Error('No neighbour found!');
 
       // Country 2
-      return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`);
+      return getJSON(
+        `https://restcountries.com/v3.1/alpha/${neighbour}`,
+        'Country not found'
+      );
     })
-    .then(response => response.json())
-    .then(data => renderCountry(data[0], 'neighbour'))
+    .then(data => {
+      renderCountry(data[0], 'neighbour');
+    })
     .catch(err => {
-      console.error(`${err} 🧨🧨🧨🧨`);
-      renderError(`Something went wrong 🧨🧨🧨🧨 ${err.message}. Try again`);
+      console.error(`${err} 💣💣💣`);
+      renderError(`Something went wrong 💣💣 ${err.message}. Try again!`);
     })
     .finally(() => {
       countriesContainer.style.opacity = 1;
@@ -137,7 +148,5 @@ const getCountryData = function (country) {
 };
 
 btn.addEventListener('click', function () {
-  getCountryData('portugal');
+  getCountryData('australia');
 });
-
-getCountryData('gagafafga');
