ffad1e4ecea3601163575404bcfb4cd4ed7a2855
Eduardo Alves <Eduardo.lves@outlook.com>
2025-12-19 20:24:42 -0300

    FixGitIgnore

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -82,7 +82,7 @@ ShoppingCart2.addToCart('piza', 2);
 console.log(ShoppingCart2);
 console.log(ShoppingCart2.shippingCost);
 */
-
+/*
 // Export
 export.addToCart = function (product, quantity) {
     cart.push({ product, quantity });
@@ -93,3 +93,20 @@ export.addToCart = function (product, quantity) {
   
 // Import
 const { addToCart } = require('./shoppingCart.js');
+*/
+
+import cloneDeep from './node_modules/lodash-es/cloneDeep.js';
+
+const state = {
+  cart: [
+    { product: 'bread', quantity: 5 },
+    { product: 'pizza', quantity: 5 },
+  ],
+  user: { loggedIn: true },
+};
+const stateClone = Object.assign({}, state);
+const stateDeepClone = cloneDeep(state);
+state.user.loggedIn = false;
+console.log(stateClone);
+
+console.log(stateDeepClone);
